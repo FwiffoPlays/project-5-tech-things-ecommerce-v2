e5d722b1f6d486200d550caca0d410cfe9511269
HarrisonBrookesBoon <harrison.m.brookes@gmail.com>
2023-10-24 18:52:27 +0000

    Add initial templates and views for basic blog functionality

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_home, name='blog_home'),
+    path('', views.blog_index, name='blog_index'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('update/<int:blog_id>/', views.update, name='update'),
+    path('delete/<int:blog_id>/', views.delete, name='delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Blog
 
-# Create your views here.
-def blog_home(request):
-    """ A view to return the 'blog_home' page """
+def blog_index(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/blog_index.html', {'blogs': blogs})
 
-    return render(request, 'blog/index.html')
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
+
+def create(request):
+    if request.method == 'POST':
+        Blog.objects.create(title=request.POST['title'], content=request.POST['content'])
+        return redirect('blog_index')
+    return render(request, 'blog/create.html')
+
+def update(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        blog.title = request.POST['title']
+        blog.content = request.POST['content']
+        blog.save()
+        return redirect('blog_index')
+    return render(request, 'blog/update.html', {'blog': blog})
+
+def delete(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    blog.delete()
+    return redirect('blog_index')
